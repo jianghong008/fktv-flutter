23607f12c59bab5cc6fbda9ae704f7ba7eba0ae0
jianghong <1637124239@qq.com>
2023-10-20 21:04:09 +0800

    gx

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:fktv/qrcode.dart';
 import 'package:fktv/top_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:flutter/services.dart';
+import 'server.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
 
   @override
@@ -30,12 +31,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   var playing = false;
+  late AppHttpServer _server;
   var controller = VideoPlayerController.networkUrl(Uri.parse(
       'https://cdn.aor.games/common/astra-web/videos/AOR-trailer.mp4'));
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    _server = AppHttpServer();
+    _server.start();
     controller.addListener(() {
       print('init');
     });
@@ -59,17 +62,19 @@ class _MyHomePageState extends State<MyHomePage> {
     // TODO: implement dispose
     super.dispose();
     controller.dispose();
+    _server.stop();
   }
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,overlays: []);
     return Scaffold(
         backgroundColor: Colors.black,
         body: Stack(
           children: [
             VideoPlayer(controller),
-            const MyTopBar(
-              title: 'hello',
+             MyTopBar(
+              title: _server.ip,
             ),
             TextButton(
                 onPressed: play, child: Text(playing ? 'pause' : 'play')),
--- lib/server.dart
+++ lib/server.dart
@@ -1,3 +1,40 @@
-class HttpServer {
-  var _server;
+import 'dart:io';
+
+class AppHttpServer {
+  late final HttpServer _server;
+  String ip = '127.0.0.1';
+  start() async {
+    ip = await getIP();
+    _server = await HttpServer.bind(InternetAddress.anyIPv4, 8848);
+    print('server is running');
+    _server.forEach((req) {
+      req.response.write('hello');
+      req.response.close();
+    });
+  }
+
+  stop() async {
+    await _server.close();
+  }
+
+  getIP() async {
+    List<NetworkInterface> list = await NetworkInterface.list();
+    List<String> ips = [];
+    for (NetworkInterface net in list) {
+      for (var ip in net.addresses) {
+        if (ip.type == InternetAddressType.IPv4 &&
+            ip.address.isNotEmpty &&
+            ip.address != '127.0.0.1') {
+          ips.add(ip.address);
+        }
+      }
+    }
+    String ip = ips.first.isEmpty ? '127.0.0.1' : ips.first;
+    for (String temp in ips) {
+      if (temp.contains('192.')) {
+        ip = temp;
+      }
+    }
+    return ip;
+  }
 }
